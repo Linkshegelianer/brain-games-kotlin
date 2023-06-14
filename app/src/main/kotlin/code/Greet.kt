e7ad0affe09af8333fbfceb9f2be06627d3000c4
Lera <mssolipsismus@gmail.com>
2023-06-14 14:59:06 +0300

    add test class

--- app/src/main/kotlin/code/Greet.kt
+++ app/src/main/kotlin/code/Greet.kt
@@ -3,12 +3,16 @@ package code
 import java.util.Scanner
 
 class Greet {
-    fun greet(): String {
+    fun greet(): String? {
         println("\n" +
                 "Welcome to the Brain Games!")
         println("May I have your name? ")
         val console = Scanner(System.`in`)
         val username = console.nextLine()
+        if (username.equals("")) {
+            println("Name cannot be empty!")
+            return null
+        }
         println("Hello, $username!")
         return username
     }
--- app/src/test/kotlin/code/AppTest.kt
+++ app/src/test/kotlin/code/AppTest.kt
@@ -1,11 +1,29 @@
 package code
 
+import code.Greet
 import code.games.Calc
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
-import kotlin.test.assertNotNull
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import java.util.*
+import kotlin.test.assertEquals
 
 class AppTest {
-    @Test fun appHasAGreeting() {
-//        assertNotNull(Calc.start(), "app should have a greeting")
+
+    @Test
+    fun testGreet() {
+        val input = ByteArrayInputStream("John".toByteArray())
+        val output = ByteArrayOutputStream()
+        System.setIn(input)
+        System.setOut(PrintStream(output))
+
+        val result = Greet().greet()
+
+        val expectedOutput = "\nWelcome to the Brain Games!\nMay I have your name? \nHello, John!\n"
+        assertEquals(expectedOutput, output.toString())
+
+        assertEquals("John", result)
     }
 }
